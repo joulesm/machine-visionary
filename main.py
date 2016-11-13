f7f2f7493a7b48d1b1173768f05a95280d6fb038
Elisha Ma <elisha@ditto.com>
2016-11-12 19:36:19 -0800

    Added some fancy ass styling to the homepage

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ def resize(width, height):
 def calculate_padding(max_size, size):
     return (max_size - size) / 2
 
+def capitalize_sentence(sentence):
+    words = sentence.split(' ')
+    words[0] = words[0].title()
+    return ' '.join(words)
+
 
 @app.route('/demotivate', methods=['POST'])
 def test():
@@ -48,16 +53,30 @@ def test():
     result = get_api_results_from_url(image_url, ['Description', 'Categories', 'Tags'], url, key)
     tags = result['description']['tags']
     print "TAGS: ", tags
+
     title = result['tags'][0]['name']
+    # Find a title that's a little more interesting
+    i = 0
+    while title in ['outdoor', 'indoor', 'person']:
+        i += 1
+        try:
+            title = result['tags'][i]['name']
+        except IndexError:
+            pass
+
+    # Resize the image to fit the poster
     width, height = resize(result['metadata']['width'], result['metadata']['height'])
+    # Calculate the necessary black padding to fill out the poster
     pad_w = calculate_padding(POSTER_W, width)
     pad_h = calculate_padding(POSTER_H, height)
-    demotivated_quote = parser.demotivate(quote['quote'], tags)
-      #return json.dumps({'tags':tags, 'title':title})
-    #return json.dumps(result)
+
+    # Get the demotivated quote and also capitalize the first letter of the sentence
+    demotivated_quote = capitalize_sentence(parser.demotivate(quote['quote'], tags))
+
     return render_template(
         'poster.html', image_url=image_url, img_h=height,
-        img_w=width, pad_w=pad_w, pad_h=pad_h, title=title.upper(), quote=demotivated_quote.capitalize())
+        img_w=width, pad_w=pad_w, pad_h=pad_h, title=title.upper(), quote=demotivated_quote,
+        orig_quote=quote['quote'], tags=tags, author=quote['author'])
 
 @app.route('/')
 def homepage():
